1ab2855aaeea87acbb49245a5853589c8fd62776
Mahek Khowala <mahekkhowala@gmail.com>
2021-05-13 15:35:09 +0530

    Forgot, reset password and update user details

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -1,5 +1,13 @@
 const express = require('express');
-const { register, login, getMe } = require('../controllers/userAuth');
+const { 
+  register, 
+  login, 
+  getMe, 
+  forgotPassword, 
+  resetPassword, 
+  updateDetails,
+  updatePassword
+} = require('../controllers/userAuth');
 
 const router = express.Router();
 
@@ -8,5 +16,9 @@ const {protect} = require('../middleware/userAuth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/update-details', protect, updateDetails);
+router.put('/update-password', protect, updatePassword);
+router.post('/forgot-password', forgotPassword);
+router.put('/reset-password/:resetToken', resetPassword);
 
 module.exports = router;
